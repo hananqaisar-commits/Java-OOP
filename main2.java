1edad4148e74a3c5b460debe241e535d97d8d64f
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-21 20:08:11 +0500

    "Code Inheritance

---
+++ main2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        children c1 = new children();
+        c1.name = "Hanan Qaisar";
+        c1.age = 17;
+        c1.school = "Comsat University Islamabad";
+        c1.Gender = "Male";
+        sc.close();
+
+        System.out.println("Name is : " + c1.name);
+        System.out.println("Institution is : " + c1.school);
+    }
+}
+
+class grandfather {
+
+    public String name;
+    public int age;
+    public String Gender;
+
+}
+
+class children extends grandfather {
+    public String school;
+    public String rollno;
+
+}

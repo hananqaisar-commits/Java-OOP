1d05e11320e96ac5e824f1c4f78451fe1d556296
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-28 22:11:46 +0500

    Debug and correct polymorphism

--- Abstraction.java
+++ Abstraction.java
@@ -1,34 +1,30 @@
 
 class Employee {
     public void claculateSalary() {
-        System.out.println("Teacher salary is calculating ");
+        System.out.print("\nTeacher salary is calculating ");
         for (int i = 0; i < 3; i++) {
-                Thread.sleep(3000);
-         catch (InterruptedException e) {
-            e.printStackTrace();
+
+            System.out.print(".");
         }
     }
-    System.out.println(".");
-}
-
-    System.out.print(("\b\b\b"));System.out.print((""));System.out.print(("\b\b\b"));
+    // System.out.print(("\b\b\b"));
+    // System.out.print((" "));
+    // System.out.print(("\b\b\b"));
 }
 
 class MathTeacher extends Employee {
-    public void teacher() {
-        System.out.println("Math teacher salary is calculating");
+    @Override
+    public void claculateSalary() {
+        System.out.println("\nMath teacher salary is calculating");
     }
 }
 
 public class Abstraction {
     public static void main(String[] args) {
 
-        Employee emp;
-
         MathTeacher e1 = new MathTeacher();
 
-        e1.claculateSalary();
-        e1.claculateSalary();
+        e1.claculateSalary();// this is called overRiding or run time polymorphisnm
 
         Employee t1 = new Employee();
         t1.claculateSalary();

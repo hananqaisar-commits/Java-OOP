a90da5e960c5f40801ed8c365b5eb3f18dc4fe6e
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-03-10 20:24:56 -0700

    Small library Book code

--- library.java
+++ library.java
@@ -4,52 +4,68 @@ public class library {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Book b1 = new Book();
+        Book books[] = new Book[5];// Here i created array only array.It will give error if i did't create objects
+                                   // of this array.
+        for (int i = 0; i < books.length; i++) {
+            books[i] = new Book();// here i am creating object of array and it is most important step
+        }
 
-                 System.out.print("Enter choice:\n1.Borrow\n2.Return");
-int choice = sc.nextInt();
+        for (int i = 0; i < books.length; i++) {
+            // input book title
+            System.out.println("Enter book " + (i + 1) + " title: ");
+            books[i].title = sc.nextLine();
 
-        switch (choice) {
-            case 1:
-                b1.borrow();
-                        b1.return;
-                break;
-            case 2:
-                b1.borrow();
-                b1.returnBook();
-                break;
-    }
-s
+            // input book author
+            System.out.println("Enter book " + (i + 1) + " author: ");
+            books[i].author = sc.nextLine();
+
+            // input book price
+            System.out.println("Enter book " + (i + 1) + " price: ");
+            double prices = sc.nextDouble();
+            books[i].setPrice(prices);
+        }
+
+        for (int i = 0; i < books.length; i++) {
+            books[i].displayInfo();
+        }
+        // check the expensive book
+        System.out.println("\nMost expensive book: " + Book.expensive(books));
+
+        // close the input scanner
         sc.close();
     }
 }
 
 class Book {
-    public String title;
     public String author;
-    private String ISBN;
+    public String title;
+    private double price;
 
-    public Book() {
-        
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public void borrow() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter borrower name: ");
-        String borrower = sc.nextLine();
-        System.out.println("Enter book name: ");
-        String book = sc.nextLine();
-
+    public double getPrice() {
+        return price;
     }
 
-    public void returnBook() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter borrower name: ");
-        String borrower = sc.nextLine();
-        System.out.println("Enter book name: ");
-        String book = sc.nextLine();
+    public void displayInfo() {
+        System.out.println("\nBook title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book price: " + price);
     }
 
+    public static double expensive(Book books[]) {
+        double expensive = books[0].getPrice();// it assingn 1st book price to expensive and then compare with other
+        for (int i = 0; i < 5; i++) {
+            if (books[i].price > expensive) {
+                expensive = books[i].price;
+            }
+        }
+        return expensive;
+    }
 }
+
+// Always remember the rule non-static feilds cannont acces in static methods
+// mean Static methods cannot directly access instance variables without an
+// object.
